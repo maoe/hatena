190b4a112004e5e7e262ef350c33ac42c04b0a77
Mitsutoshi Aoe <maoe@foldr.in>
2012-03-12 23:17:23 +0900

    Use Web.Hatena.Internal in Star API

--- src/Web/Hatena/Star.hs
+++ src/Web/Hatena/Star.hs
@@ -11,31 +11,28 @@ import qualified Data.ByteString.Char8 as BS (unpack)
 import Control.Failure (Failure)
 import Control.Monad.Trans.Resource (ResourceIO)
 import Data.Conduit (($$))
-import Data.Conduit.Attoparsec
 import Network.HTTP.Conduit
 import Network.HTTP.Types (Ascii)
 import qualified Data.Aeson as A
 import qualified Data.Aeson.TH as A (deriveJSON)
 
 import Web.Hatena.Monad
+import Web.Hatena.Internal (jsonResponse, camelToSnake)
 
 data StarCount = StarCount
-  { title      :: Text
-  , star_count :: Int
-  , uri        :: Ascii
-  , count      :: A.Object
+  { starCountTitle :: Text
+  , starCount      :: Int
+  , starCountUri   :: Ascii
+  , starCountCount :: A.Object
   } deriving Show
 
-$(A.deriveJSON id ''StarCount)
+$(A.deriveJSON (camelToSnake "starCount") ''StarCount)
 
 getStarCount :: (ResourceIO m, Failure HttpException m)
              => Ascii -> HatenaT noauth m StarCount
 getStarCount uri = do
   req <- lift $ parseUrl $ "http://s.hatena.ne.jp/blog.json/" ++ BS.unpack uri
-  manager <- getManager
-  json <- lift $ do
-    resp <- http req manager
-    responseBody resp $$ sinkParser A.json
+  json <- jsonResponse req
   case A.fromJSON json of
     A.Success starCount -> return starCount
     A.Error reason      -> fail reason
