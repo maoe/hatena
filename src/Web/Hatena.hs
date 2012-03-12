2dd9f6d4a5cf9ca34a474650406ce93c51f27297
Mitsutoshi Aoe <maoe@foldr.in>
2012-03-12 23:46:39 +0900

    Export Web.Hatena.Star from Web.Hatena

--- src/Web/Hatena.hs
+++ src/Web/Hatena.hs
@@ -2,8 +2,10 @@ module Web.Hatena
   ( module Web.Hatena.Auth
   , module Web.Hatena.Monad
   , module Web.Hatena.OAuth
+  , module Web.Hatena.Star
   ) where
 
 import Web.Hatena.Auth
 import Web.Hatena.Monad
 import Web.Hatena.OAuth
+import Web.Hatena.Star
