703f77b43836ec5f0b8a55134d6c7310d935098b
Mitsutoshi Aoe <maoe@foldr.in>
2012-03-20 00:35:34 +0900

    Add postBookmark

--- src/Web/Hatena/Bookmark.hs
+++ src/Web/Hatena/Bookmark.hs
@@ -1,23 +1,27 @@
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
 module Web.Hatena.Bookmark
   ( getRootAtomEndPoints
   , AtomEndPoint(..)
+  , postBookmark
   ) where
 import Control.Applicative ((<$>))
 import Control.Monad.Trans (lift)
 import Data.Maybe (listToMaybe)
 import Data.Text (Text)
+import qualified Data.Text as T
 
 import Control.Failure (Failure)
 import Control.Monad.Trans.Resource (ResourceIO)
 import Network.HTTP.Conduit (HttpException, parseUrl)
+import Text.Hamlet.XML (xml)
 import Text.XML (Document)
 import Text.XML.Cursor (($//), (>=>), fromDocument, attributeIs, attribute)
 
 import Web.Hatena.Auth
 import Web.Hatena.Monad
-import Web.Hatena.Internal (atomResponse)
+import Web.Hatena.Internal (postAtom, atomResponse)
 
 getRootAtomEndPoints
   :: (ResourceIO m, Failure HttpException m)
@@ -36,9 +40,34 @@ fromAtom atom = listToMaybe $ zip (AtomEndPoint <$> postEndPoints)
   where
     cursor = fromDocument atom
     postEndPoints = cursor $// "rel" `attributeIs` "service.post"
-                           >=> attribute "title"
+                           >=> attribute "href"
     feedEndPoints = cursor $// "rel" `attributeIs` "service.feed"
-                           >=> attribute "title"
+                           >=> attribute "href"
 
 newtype AtomEndPoint = AtomEndPoint { atomEndPoint :: Text }
   deriving Show
+
+postBookmark
+  :: (ResourceIO m, Failure HttpException m)
+  => Text -- ^ URI
+  -> Text -- ^ Comments
+  -> HatenaT (Either (OAuth scope) WSSE) m AtomEndPoint
+postBookmark uri summary = do
+  (postEndPoint, _) <- getRootAtomEndPoints
+  req <- postAtom (T.unpack (atomEndPoint postEndPoint))
+                  [xml|
+                    <link rel=related type=text/html href=#{uri}>
+                    <summary type=text/plain>#{summary}
+                  |]
+  req' <- authenticate req
+  atom <- atomResponse req'
+  case fromAtom' atom of
+    Just endPoint -> return endPoint
+    Nothing       -> fail "failed"
+
+fromAtom' :: Document -> Maybe AtomEndPoint
+fromAtom' atom = listToMaybe $ (AtomEndPoint <$> editEndPoint)
+  where
+    cursor = fromDocument atom
+    editEndPoint = cursor $// "rel" `attributeIs` "service.edit"
+                          >=> attribute "href"
--- src/Web/Hatena/Internal.hs
+++ src/Web/Hatena/Internal.hs
@@ -1,19 +1,36 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Web.Hatena.Internal
-  ( jsonResponse
+  ( postAtom
+  , jsonResponse
   , atomResponse
   , camelToSnake
   ) where
-import Control.Monad.Trans (lift)
+import Control.Monad.Trans (lift, liftIO)
 import Data.Char (toLower, isUpper)
 import Data.List (stripPrefix)
+import qualified Data.Text.Lazy.IO as T
 
+import Control.Failure (Failure)
 import Data.Aeson (Value, json)
 import Data.Conduit (ResourceIO, ($$))
 import Data.Conduit.Attoparsec (sinkParser)
-import Network.HTTP.Conduit (Request, http, responseBody)
-import Text.XML (Document, sinkDoc, def)
+import Network.HTTP.Conduit -- (Request, http, responseBody)
+import Text.XML (Document(..), Prologue(..), Element(..), Node, sinkDoc, renderLBS, renderText)
 
 import Web.Hatena.Monad (HatenaT, getManager)
+
+postAtom :: (ResourceIO m, Failure HttpException m)
+         => String -> [Node] -> HatenaT auth m (Request m)
+postAtom uri nodes = do
+  req <- lift $ parseUrl uri
+  return req
+    { method      = "POST"
+    , requestBody = RequestBodyLBS $ renderLBS def doc
+    }
+  where
+    doc = Document (Prologue [] Nothing []) root []
+    root = Element "entry" [("xmlns", "http://purl.org/atom/ns#")] nodes
 
 jsonResponse :: ResourceIO m => Request m -> HatenaT auth m Value
 jsonResponse req = do
