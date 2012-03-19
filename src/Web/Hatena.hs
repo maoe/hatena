82adb3f3abd357c71db7764aaed74b3a43cdb03a
Mitsutoshi Aoe <maoe@foldr.in>
2012-03-20 00:53:37 +0900

    Remove Star and OAuth from Web.Hatena

--- src/Web/Hatena.hs
+++ src/Web/Hatena.hs
@@ -1,11 +1,8 @@
 module Web.Hatena
   ( module Web.Hatena.Auth
   , module Web.Hatena.Monad
-  , module Web.Hatena.OAuth
-  , module Web.Hatena.Star
   ) where
 
 import Web.Hatena.Auth
 import Web.Hatena.Monad
-import Web.Hatena.OAuth
-import Web.Hatena.Star
+
